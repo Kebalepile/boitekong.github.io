f81d2f5f743e500b4721cc4fd1f867904213054e
rustybiskop <rustybiskop@gmail.com>
2023-09-19 15:35:25 +0200

    log print added

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import(
 	pub "github.com/Kebalepile/job_board/spiders/public"
 )
 func main(){
-	log.Println("Job Board Scrapper Initiated !")
+	log.Println("Job Board Scrapper Initiated ")
 	govpageSpider := pub.Spider{
 		Name: "gov-page",
 		AllowedDomains: []string{"https://www.govpage.co.za/",
