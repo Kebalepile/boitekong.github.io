99149a528c66138cc1c4a072af24a594576a993c
rustybiskop <rustybiskop@gmail.com>
2023-10-07 19:04:44 +0200

    python types

---
+++ spiders/types/types.py
@@ -0,0 +1,20 @@
+
+# Used in Govpage spider
+def Links():
+    return {
+        "Title": "",
+        "BlogPosts": [],
+        "Departments": {}
+
+    }
+# Used in Govpage spider
+
+
+def BlogPost():
+    return {
+        "Href": "",
+        "Title": "",
+        "Content": [],
+        "PostedDate": "",
+        "Iframe": "",
+    }
