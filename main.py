f9f100239e7c2f4a201453a64c6519852a5f2cf9
Kebalepile <pimpabutterfly86@gmail.com>
2023-10-10 09:55:22 +0200

    python main and updated go compiled main.exe

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# from spiders.public.govpage.govpageSpider import Spider
+from spiders.public.govpage.govpageSpider import Spider
 from spiders.private.govpage.govpageSpider import Spider as PrivateSpider
 
 
 def main():
 
-    # govpage_spider = Spider()
-    # govpage_spider.Launch()
+    govpage_spider = Spider()
+    govpage_spider.Launch()
 
     govpagePrivateSector = PrivateSpider()
     govpagePrivateSector.Launch()
