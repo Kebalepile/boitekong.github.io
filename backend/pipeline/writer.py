9f8ac37478c3ebe65328a4fa5905d301286f113d
Kebalepile <pimpabutterfly86@gmail.com>
2023-10-10 11:17:54 +0200

    moved files to backend folder'

---
+++ backend/pipeline/writer.py
@@ -0,0 +1,10 @@
+# import os
+import json
+
+def GovPageFile(data: dict):
+    path = f'database/public/{data["Title"]}.json'
+    with open(path,"w") as f:
+        json.dump(data,f, indent=4)
+        print(f"data saved to: {path}")
+
+
---
+++ backend/spiders/types/types.py
@@ -0,0 +1,21 @@
+
+# Used in Govpage spider
+def Links():
+    return {
+        "Title": "",
+        "BlogPosts": [],
+        "Departments": {}
+
+    }
+# Used in Govpage spider
+
+
+def BlogPost():
+    return {
+        "ImgSrc":"",
+        "Href": "",
+        "Title": "",
+        "Content": [],
+        "PostedDate": "",
+        "Iframe": "",
+    }
