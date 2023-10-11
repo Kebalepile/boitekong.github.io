eb6cec0e50c7ea6e153f2ee85fa2336a011d7062
Kebalepile <pimpabutterfly86@gmail.com>
2023-10-11 15:56:01 +0200

    init headline component for frontend.

---
+++ components/headline.js
@@ -0,0 +1,12 @@
+import propersonnelData from "../backend/database/private/Pro-Personnel.json";
+import heithaData from "../backend/database/private/heitha-stuffing-group.json";
+// import privateSectorData from "../backend/database/public/govpage-private-sector.json"
+import publicSectorData from "../backend/database/public/govpage-public-sector.json"
+
+export function setupHeadline(element) {
+  
+  element.innerHTML = `
+          <li> ${publicSectorData.BlogPosts.length} Government Departments hiring</li>
+          <li>n Private Sector Entities/Companies hiring</li>
+          <li> ${propersonnelData.blogPosts.length + heithaData.blogPosts.length} Private Sector Job openings</li>`;
+}
