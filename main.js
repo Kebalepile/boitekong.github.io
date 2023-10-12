123e2f3ad008e8c61ec8005d5746b0fc4120bfe9
Kebalepile <pimpabutterfly86@gmail.com>
2023-10-12 13:51:04 +0200

    connect privateCompanies component

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 import "./style.css";
 
-import { setupHeadline } from "./components/headline";
+import { setupHeadline } from "./components/headline.js";
+import {setupPrivateCompanies} from "./components/boards/privateCompanies.js";
 
 document.querySelector("#app").innerHTML = `
+  <header></header>
   <div id="headline">
-  
     <h1>Hello Mereko</h1>
     <div id="headline-message" class="card">
        <h3>Looking for a job in the South African Market</h3>
        <br/>
        <h4>Start Here</h4>
        <p>Currently There Are :</p>
-        <ul id="headline-job-info">
-             
-        </ul>
-      
+        <ul id="headline-job-info"></ul> 
     </div>
-    
   </div>
+  <div id="job-board"></div>
+  <footer></footer>
 `;
 
 setupHeadline(document.querySelector("#headline-job-info"));
+setupPrivateCompanies(document.querySelector("#job-board"))
